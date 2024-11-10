e62949329cea85b43daf5f16864fe22825f07582
mossivy <hmossjvy@gmail.com>
2024-11-10 12:28:38 -0500

    basic ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Fullscreen } from "@react-three/uikit";
 import { Defaults } from "@react-three/uikit-apfel";
 import { Card } from '@react-three/uikit-apfel';
 import { Container, Text } from "@react-three/uikit";
+import {Suspense} from 'react';
 
 function UiElement() {
   return (
@@ -14,18 +15,29 @@ function UiElement() {
   )
 }
 
+// New Sphere component
+function Sphere() {
+  return (
+    <mesh position={[0, 0, 0]}>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color="royalblue" />
+    </mesh>
+  )
+}
+
 export default function App() {
   return (
-    <Canvas style={{ position: "absolute", inset: "0", touchAction: "none" }} gl={{ localClippingEnabled: true }}>
+    <Canvas style={{ position: "absolute", inset: "0", touchAction: "none" }} gl={{ localClippingEnabled: true }}>  
       <ambientLight intensity={0.5} />
       <directionalLight intensity={1} position={[-5, 5, 10]} />
+      <Sphere/>
       <Defaults>
         <Fullscreen
         overflow="scroll"
         scrollbarColor="black"
         backgroundColor="white"
         dark={{ backgroundColor: "black" }}
-u       flexDirection="column"
+        flexDirection="column"
         gap={32}
         paddingX={32}
         alignItems="center"
