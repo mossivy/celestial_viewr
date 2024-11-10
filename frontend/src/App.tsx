a6dc1011f7badbf058962c3b997342b0764c1b24
mossivy <hmossjvy@gmail.com>
2024-11-10 17:12:32 -0500

    box shows up but camera doesnt work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import React, { useState, useEffect} from 'react';
+import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { Fullscreen } from "@react-three/uikit";
 import { Defaults } from "@react-three/uikit-apfel";
 import { Card } from '@react-three/uikit-apfel';
 import { Container, Text } from "@react-three/uikit";
-import {Suspense} from 'react';
+import { OrbitControls } from '@react-three/drei';
 
 function UiElement() {
   return (
@@ -15,37 +17,52 @@ function UiElement() {
   )
 }
 
-// New Sphere component
-function Sphere() {
-  return (
-    <mesh position={[0, 0, 0]}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color="royalblue" />
-    </mesh>
-  )
+export default function App() {
+
+  const [objects, setObjects] = useState([]);
+
+// Function to add a new box to the scene
+  function addObject() {
+  // Add a new box with a random position and a unique id
+    setObjects([
+      ...objects,
+    {
+      id: Date.now(), // Unique ID
+      position: [Math.random() * 100 - 5, Math.random() * 100 - 5, Math.random() * 100 - 5],
+      color: new THREE.Color(Math.random(), Math.random(), Math.random()).getHex() // Random color
+    }
+  ])
 }
 
-export default function App() {
+ 
+
   return (
-    <Canvas style={{ position: "absolute", inset: "0", touchAction: "none" }} gl={{ localClippingEnabled: true }}>  
-      <ambientLight intensity={0.5} />
-      <directionalLight intensity={1} position={[-5, 5, 10]} />
-      <Sphere/>
-      <Defaults>
+    <Canvas 
+      camera={{ position: [0, -10, 20], fov: 32.5 }}
+      style={{width: '100vw', height: '100vw', position: 'absolute', top: 0, left: 0, touchAction: "none" }} 
+      gl={{ localClippingEnabled: true }}>  
+
+      <OrbitControls target={[0, 0, 0]}/>
+              <Defaults>
+        <ambientLight intensity={0.5} />
+        <directionalLight intensity={1} position={[0, 5, 10]} />
         <Fullscreen
-        overflow="scroll"
-        scrollbarColor="black"
-        backgroundColor="white"
-        dark={{ backgroundColor: "black" }}
-        flexDirection="column"
-        gap={32}
-        paddingX={32}
-        alignItems="center"
-        padding={32}>
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="center"
+          paddingTop={25}
+          paddingBottom={0}>
+          
           <UiElement/>
+
         </Fullscreen>
-      </Defaults>
-    </Canvas>
+          <mesh position={[0,0,0]} >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial/>
+          </mesh>
+        <axesHelper args={[5]} />
+    </Defaults>
+  </Canvas>
   )
 }
 
