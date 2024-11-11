36eb1e756419339a8a42ad948d9d74a75e48d4cd
mossivy <hmossjvy@gmail.com>
2024-11-11 14:15:09 -0500

    ui and camera works correctly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect} from 'react';
-import * as THREE from 'three';
-import { Canvas } from '@react-three/fiber';
+import React, { useState, useEffect } from "react";
+import * as THREE from "three";
+import { Canvas, useThree } from "@react-three/fiber";
 import { Fullscreen } from "@react-three/uikit";
 import { Defaults } from "@react-three/uikit-apfel";
-import { Card } from '@react-three/uikit-apfel';
+import { Card } from "@react-three/uikit-apfel";
 import { Container, Text } from "@react-three/uikit";
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls } from "@react-three/drei";
 
 function UiElement() {
   return (
@@ -13,57 +13,87 @@ function UiElement() {
       <Card borderRadius={32} padding={16}>
         <Text>Tester Text!!</Text>
       </Card>
-   </Container>
-  )
+    </Container>
+  );
+}
+
+function Camera() {
+  const { gl, camera } = useThree();
+
+  useEffect(() => {
+    const handleResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      gl.setSize(window.innerWidth, window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Should fix the centering issue
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [gl, camera]);
+
+  return null;
 }
 
 export default function App() {
-
   const [objects, setObjects] = useState([]);
 
-// Function to add a new box to the scene
   function addObject() {
-  // Add a new box with a random position and a unique id
     setObjects([
       ...objects,
-    {
-      id: Date.now(), // Unique ID
-      position: [Math.random() * 100 - 5, Math.random() * 100 - 5, Math.random() * 100 - 5],
-      color: new THREE.Color(Math.random(), Math.random(), Math.random()).getHex() // Random color
-    }
-  ])
-}
+      {
+        id: Date.now(), // Unique ID
+        position: [0, 0, 0],
+        color: new THREE.Color(
+          Math.random(),
+          Math.random(),
+          Math.random()
+        ).getHex(), // Random color
+      },
+    ]);
+  }
 
- 
+  useEffect(() => {
+    addObject();
+  }, []);
 
   return (
-    <Canvas 
-      camera={{ position: [0, -10, 20], fov: 32.5 }}
-      style={{width: '100vw', height: '100vw', position: 'absolute', top: 0, left: 0, touchAction: "none" }} 
-      gl={{ localClippingEnabled: true }}>  
-
-      <OrbitControls target={[0, 0, 0]}/>
-              <Defaults>
-        <ambientLight intensity={0.5} />
-        <directionalLight intensity={1} position={[0, 5, 10]} />
+    <Canvas
+      camera={{ position: [0, -10, 50], fov: 32.5 }}
+      style={{
+        width: "100vw",
+        height: "100vw",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        touchAction: "none",
+      }}
+      gl={{ localClippingEnabled: true }}
+    >
+      <Camera />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={1} position={[0, 5, 10]} />
+      <OrbitControls target={[0, 0, 0]} />
+      <Defaults>
         <Fullscreen
           flexDirection="column"
           justifyContent="flex-start"
           alignItems="center"
           paddingTop={25}
-          paddingBottom={0}>
-          
-          <UiElement/>
-
+          paddingBottom={0}
+        >
+          <UiElement />
         </Fullscreen>
-          <mesh position={[0,0,0]} >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial/>
-          </mesh>
-        <axesHelper args={[5]} />
-    </Defaults>
-  </Canvas>
-  )
+      </Defaults>
+      {objects.map((object) => (
+        <mesh key={object.id} position={object.position}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={object.color} />
+        </mesh>
+      ))}
+      <axesHelper args={[5]} />
+    </Canvas>
+  );
 }
 
 // Lighting setup inside Canvas component
